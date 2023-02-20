a4560f88a3419618e59452e41847d3c16548e9d8
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-02-20 13:26:22 +0100

    Update registreren.php

--- registreren.php
+++ registreren.php
@@ -2,13 +2,13 @@
 require_once "database.php";
 if ($_POST) {
     $pass = password_hash($_POST["wachtwoord"], PASSWORD_DEFAULT);
-$sql = "INSERT INTO gebruiker (voornaam, achternaam, gebruikersnaam, wachtwoord, id_land) values(?,?,?,?,?)";
+    $sql = "INSERT INTO gebruiker (voornaam, achternaam, gebruikersnaam, wachtwoord, id_land) values(?,?,?,?,?)";
     $stmt = $con->prepare($sql);
     $stmt->bindValue(1, $_POST["voornaam"]);
     $stmt->bindValue(2, $_POST["achternaam"]);
     $stmt->bindValue(3, $_POST["gebruikersnaam"]);
     $stmt->bindValue(4, $pass);
-$stmt->bindValue(5, $_POST["land_id"]);
+    $stmt->bindValue(5, $_POST["land_id"]);
 
     $stmt->execute();
 
@@ -25,16 +25,12 @@ $stmt->bindValue(5, $_POST["land_id"]);
 
 <body>
     <form method="POST">
-        <container class="container">
-            <input type="text" class="voornaam" name="voornaam" placeholder="Voornaam"><br>
-            <input type="text" class="achternaam" name="achternaam" placeholder="Achternaam"><br>
-            <input type="text" class="gebruikersnaam" name="gebruikersnaam" placeholder="Gebruikersnaam"><br>
-            <input type="password" class="wachtwoord" name="wachtwoord" placeholder="Wachtwoord"><br>
-            Admin: <input type="checkbox" class="admin" name="admin"><br>
-            <input type="text" class="land" name="land_id" placeholder="Land"><br>
-            <!--href locatie toevoegen naar homepagina-->
-            <button type="submit" class="btn btn-primary" href="registreren.php">Registreren</button><br>
-        </container>
+        <input type="text" class="voornaam" name="voornaam" placeholder="Voornaam"><br>
+        <input type="text" class="achternaam" name="achternaam" placeholder="Achternaam"><br>
+        <input type="text" class="gebruikersnaam" name="gebruikersnaam" placeholder="Gebruikersnaam"><br>
+        <input type="password" class="wachtwoord" name="wachtwoord" placeholder="Wachtwoord"><br>
+        <input type="text" class="land" name="land_id" placeholder="Land"><br>
+        <button type="submit" class="btn btn-primary">Registreren</button><br>
     </form>
 </body>
 
