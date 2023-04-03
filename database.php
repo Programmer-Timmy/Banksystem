d48a0c2ea9e99f8e680a842653063a79b85632b4
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-04-03 10:01:06 +0200

    Update database.php

--- database.php
+++ database.php
@@ -2,4 +2,3 @@
 $con = new PDO("mysql:host=portfolio.ictcampus.nl;dbname=murcs_budget", "gdb_murcs_budget", "9I0!s3aV");
 $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-session_start();
