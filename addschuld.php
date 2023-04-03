48d8dea87af0f52ede76beb8ec35012ee33308b4
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-04-03 10:00:55 +0200

    Update addschuld.php

--- addschuld.php
+++ addschuld.php
@@ -1,25 +1,26 @@
 <?php
 date_default_timezone_set("Europe/Amsterdam");
+session_start();
 require 'database.php';
 
 
 //JSON CodE
 if ($_POST) {
-  $stmt = $conn->prepare("INSERT INTO schuld(waarde, datum_schuld, waarborg, id_schuld_soort, id_gebruiker) VALUES (?, ?, ?, ?, ?)");
+  $stmt = $con->prepare("INSERT INTO schuld(waarde, datum_schuld, waarborg, id_schuld_soort, id_gebruiker) VALUES (?, ?, ?, ?, ?)");
 
   $stmt->bindValue(1, $_POST['waarde']);
-  $stmt->bindValue(2, date("d-m-Y H:i:s"));
-  if($_POST['waarborg'] == 1){
+  $stmt->bindValue(2, date('d-m-y H:i:s'));
+  if(isset($_POST['waarborg'])){
   $stmt->bindValue(3, 1);
   }
   else {
     $stmt->bindValue(3, 0);
   }
   $stmt->bindValue(4, $_POST['soort']);
-  $stmt->bindValue(5, 1);
+  $stmt->bindValue(5, $_SESSION['id']);
   $stmt->execute();
 
-  header("location: index.php");
+  header("location: portal.php");
 }
 
 
