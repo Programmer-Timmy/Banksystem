2030a76505172bbeedd35c0c5d5d205b9fffee34
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-04-03 08:39:21 +0200

    Create activa.php

---
+++ activa.php
@@ -0,0 +1,91 @@
+<?php
+require 'database.php';
+
+if(isset($_GET["id"])) {
+    $stmt = $conn->prepare("DELETE FROM activa WHERE id_activa = ?");
+    $stmt->bindValue(1, $_GET["id"]);
+    
+    $stmt->execute();
+    header("location: activa.php");
+}
+
+$stmt = $conn->prepare(
+"SELECT activa.id_activa, activa.waarde, datum_aankoop, activa.materieel, activa.id_gebruiker, activa_soort.soort
+FROM activa
+JOIN activa_soort ON activa.id_activa_soort = activa_soort.id_activa_soort
+WHERE id_gebruiker = 1;;");
+$stmt->execute();
+
+$tests = $stmt->fetchAll(PDO::FETCH_OBJ);
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+    <div class="header">
+        <a href="#default" class="logo">    <?php
+    echo ("Hallo $gebruiker->voornaam $gebruiker->achternaam");
+
+    ?></a>
+        <div class="header-right">
+            <a href="index.php">Home</a>
+            <a href="inkomsten.php">Inkomsten</a>
+            <a href="uitgaven.php">Uitgaven</a>
+            <a href="schulden.php">Schulden</a>
+            <a class="active" href="activa.php">Activa</a>
+
+
+        </div>
+    </div>
+
+    <br><br>
+
+    <a href="addactiva.php" class="button">Transactie toevoegen</a>
+
+
+    <h1>activa</h1>
+    <table class='table table-striped'>
+        <thead class='table-dark>'>
+            <th>Waarde</th>
+            <th>Datum</th>
+            <th>Soort</th>
+            <th>Materieel</th>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($tests as $test) {
+                echo "<tr>";
+                echo "<td>€$test->waarde</td>";
+                echo "<td>$test->datum_aankoop</td>";
+                echo "<td>$test->soort</td>";
+
+                if ($test->materieel == 1) {
+                    echo "<td>Ja</td>";
+                } else {
+                    echo "<td>Nee</td>";
+                }
+
+                echo "<td><a class='btn btn-danger' href='inkomsten.php?id=$test->id_activa' onclick='return confirm(\"Weet je het zeker?\");'>X</a></td>";
+
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+ 
+</body>
+
+</html>
