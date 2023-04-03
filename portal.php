b9adac0e9029e1397eebe70933a7aa1555152583
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-04-03 08:39:47 +0200

    Update portal.php

--- portal.php
+++ portal.php
@@ -1,25 +1,53 @@
 <?php
 
-session_start();
 
-if(!isset($_SESSION["id"])) {
-    // header("location: index.php");
-}
-
-//var_dump($_SESSION);
+$gebruiker = new stdClass();
+$gebruiker->id_gebruiker = 1;
+$gebruiker->voornaam = "Jesse";
+$gebruiker->achternaam = "van Doorn";
 
 ?>
-<a href="logout.php">Afmelden</a>
+
+<!DOCTYPE html>
 <html>
+
 <head>
-<link rel="stylesheet" href="style.css">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
+
 <body>
-<div class="container">
-<h2>Saldo
-<h2>€123.456.789,00</h2>
-<button type="submit" class="button2">overmaken</button><br>
-</div>
+    <div class="header">
+        <a href="#default" class="logo"> <?php
+                                            echo ("Hallo $gebruiker->voornaam $gebruiker->achternaam");
+
+                                            ?></a>
+        <div class="header-right">
+            <a class="active" href="index.php">Home</a>
+            <a href="inkomsten.php">Inkomsten</a>
+            <a href="uitgaven.php">Uitgaven</a>
+            <a href="schulden.php">Schulden</a>
+            <a href="activa.php">Activa</a>
+
+
+        </div>
+    </div>
+
+    <br><br>
+    <button class="button-36" onclick="window.location.href='transactie.php'">Transactie toevoegen</button>
+    <button class="button-36" onclick="window.location.href='addschuld.php'">Schuld toevoegen</button>
+    <button class="button-36" onclick="window.location.href='addactiva.php'">Activa toevoegen</button>
+
+
+
+
+
+
 </body>
 
 </html>
