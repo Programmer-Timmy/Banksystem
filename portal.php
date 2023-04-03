f40ddb858274418e297e81f3140fe0ed0dcaecdf
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-04-03 12:18:33 +0200

    Update portal.php

--- portal.php
+++ portal.php
@@ -23,6 +23,8 @@ $gebruiker = $stmt->fetchObject();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
+
 
 </head>
 
