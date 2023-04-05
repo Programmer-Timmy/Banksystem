97032beb98e0435492c24e566de4eefc64af98b8
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-04-05 10:51:53 +0200

    Update addactiva.php

--- addactiva.php
+++ addactiva.php
@@ -11,11 +11,10 @@ if ($_POST) {
   $stmt = $con->prepare("INSERT INTO activa(waarde, datum_aankoop, materieel, id_activa_soort, id_gebruiker) VALUES (?, ?, ?, ?, ?)");
 
   $stmt->bindValue(1,  $_POST['waarde']);
-  $stmt->bindValue(2, date("y-m-d H:i:s"));
-  if(isset($_POST['materieel'])){
-  $stmt->bindValue(3, 1);
-  }
-  else {
+  $stmt->bindValue(2, $_POST['date']);
+  if (isset($_POST['materieel'])) {
+    $stmt->bindValue(3, 1);
+  } else {
     $stmt->bindValue(3, 0);
   }
   $stmt->bindValue(4, $_POST['soort']);
@@ -33,66 +32,73 @@ if ($_POST) {
 
 <!DOCTYPE html>
 <html>
-    <head>
-        
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <style>
-            #atakan {
-                width: 500px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class='container-fluid'>
-            <div class="card mx-auto" id="atakan">
-                <div class="card-header">
-                  Activa toevegen
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title"></h5>
-                  
-                  <form method="post">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Waarde in Euro's</label>
-                            <input type="number" class="form-control" id="waarde" name="waarde" aria-describedby="emailHelp" step=".01">
-                        </div>
 
-                        <input type="checkbox" id="materieel" name="materieel" value="1">
-                        <label for="waarborg">Het is materieel:</label><br>
+<head>
 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <style>
+    #atakan {
+      width: 500px;
+    }
+  </style>
+</head>
 
-                        <strong>Soort Activa</strong>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="soort" id="exampleRadios1" value="1" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                            Auto
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="soort" id="exampleRadios1" value="2" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                            Huis
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="soort" id="exampleRadios1" value="3" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                            Aandelen
-                            </label>
-                        </div>
-                        <div class="form-check mb-4">
-                            <input class="form-check-input" type="radio" name="soort" id="exampleRadios1" value="4" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                              Anders
-                            </label>
-                            
-                        </div>
-                        <input type="submit" value="Submit">
+<body>
+  <div class='container-fluid'>
+    <div class="card mx-auto" id="atakan">
+      <div class="card-header">
+        Activa toevegen
+      </div>
+      <div class="card-body">
+        <h5 class="card-title"></h5>
 
-                  </form>
+        <form method="post">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Waarde in Euro's</label>
+            <input type="number" class="form-control" id="waarde" name="waarde" aria-describedby="emailHelp" step=".01">
+          </div>
 
-                </div>
-              </div>
-        </div>
-    </body>
+          <input type="checkbox" id="materieel" name="materieel" value="1">
+          <label for="waarborg">Het is materieel:</label><br>
+
+          <div class="form-group">
+            <label for="exampleInputEmail1">datum</label>
+            <input type="date" class="form-control" id="date" name="date" aria-describedby="emailHelp" step=".01">
+          </div>
+
+          <strong>Soort Activa</strong>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="soort" id="exampleRadios1" value="1" checked>
+            <label class="form-check-label" for="exampleRadios1">
+              Auto
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="soort" id="exampleRadios1" value="2" checked>
+            <label class="form-check-label" for="exampleRadios1">
+              Huis
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="soort" id="exampleRadios1" value="3" checked>
+            <label class="form-check-label" for="exampleRadios1">
+              Aandelen
+            </label>
+          </div>
+          <div class="form-check mb-4">
+            <input class="form-check-input" type="radio" name="soort" id="exampleRadios1" value="4" checked>
+            <label class="form-check-label" for="exampleRadios1">
+              Anders
+            </label>
+
+          </div>
+          <input type="submit" value="Submit">
+
+        </form>
+
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
